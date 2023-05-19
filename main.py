6152ddb141299251428da18e324c3c106c00ac3c
Asser <assertamer2002@gmail.com>
2023-05-19 15:43:08 +0300

    a5ry gab a5ro

--- main.py
+++ main.py
@@ -27,39 +27,56 @@ class Graph():
         return self.graph[current.index][destination.index]
 
     def initializeSource(self,startIndex):
+        copy=[]
         for v in range(self.verticesCount):
             vertex=Vertex()
             vertex._init_(v)
             self.verticesList.append(vertex)
+            copy.append(vertex)
         self.verticesList[startIndex].modify(0,None)
+        copy[startIndex].modify(0,None)
+        return copy
         
 
     def Relax(self,minVertex,destination):
         if destination.distance>minVertex.distance+self.Weight(minVertex,destination):
+            
             destination.modify(minVertex.distance+self.Weight(minVertex,destination),minVertex)
-            if self.directed==False:
-                destination.modify(minVertex.distance+self.Weight(minVertex,destination),minVertex)
-            return True
+            # print(f"new distance of {destination.index} is {destination.distance}")
+
  
 
         
-    def shortestPath(self,startIndex):
-        path=[]
+    def shortestPath(self,startIndex,endIndex):
         counter=self.verticesCount   
-        self.initializeSource(startIndex)
+        copy=self.initializeSource(startIndex)
         self.BUILD_MIN_HEAP()
         while self.verticesCount != 0:
             minVertex=self.ExtractMin()
-            path.append(minVertex)
+            if minVertex.index==startIndex:
+                startVertex=minVertex
+            elif minVertex.index==endIndex:
+                    endvertex=minVertex
             for i in range(counter):
                 if self.graph[minVertex.index][i] !=0:
-                    self.Relax(minVertex,self.verticesList[i])
-        
-                        
-                        
-        # print("Shortest :")
-        for i in range(len(path)):
-            print(f"index --> {path[i].index}   distance {path[i].distance}")
+                    self.Relax(minVertex,copy[i])
+        self.printPath(startVertex,endvertex)
+
+    def printPath(self,startvertex,endvertex):
+        if startvertex==endvertex:
+            return
+        current=endvertex
+        stack=[]
+        stack.append(endvertex)
+        while current!= startvertex:
+            # print(f"putting {current.index} of pre= {current.predecessor.index}")
+            stack.append(current.predecessor)
+            current=current.predecessor
+        for i in range(len(stack)):
+            vertex=stack.pop()
+            print(f"to index -> {vertex.index} of distance {vertex.distance}")
+
+                    
 
 
     def MIN_HEAPIFY(self, i):
@@ -89,56 +106,68 @@ class Graph():
 
     def primMST(self):
         weights = [100000]*self.verticesCount
-        parent = [None] * self.verticesCount
+        pred = [None] * self.verticesCount
 
         weights[0]=0
-        mstSet=[False] * self.verticesCount
+        visited=[False] * self.verticesCount
 
         #set as root
-        parent[0]= -1
+        pred[0]= -1
 
         for cout in range (self.verticesCount):
             #find edge with smallest weight
-            u=self.minWeight(weights,mstSet)
+            u=self.minWeight(weights,visited)
 
-            mstSet[u]=True
+            #Set Vertix as Visited
+            visited[u]=True
 
+            # Update the distance to reach adjecent Vertices and setting its predecessor
             for v in range(self.verticesCount):
-                if self.graph[u][v] > 0 and mstSet[v]== False  and weights[v]>self.graph[u][v]:
+                if self.graph[u][v] > 0 and visited[v]== False  and weights[v]>self.graph[u][v]:
                     weights[v]=self.graph[u][v]
-                    parent[v]=u
-        self.printMST(parent)
+                    pred[v]=u
+        self.printMST(pred)
 
-    def minWeight(self,weight,mstSet):
+    def minWeight(self,weight,visited):
 
         min = 10000000
 
         for v in range(self.verticesCount):
-            if weight[v] < min and mstSet[v] == False:
+            if weight[v] < min and visited[v] == False:
                 min = weight[v]
                 min_index = v
 
         return min_index
 
-    def printMST(self, parent):
+    def printMST(self, pred):
         print("Edge \tWeight")
         for i in range(1, self.verticesCount):
-            print(parent[i], "-", i, "\t", self.graph[i][parent[i]])
+            print(pred[i], "-", i, "\t", self.graph[i][pred[i]])
+
 
 
 #Test 
 G=Graph()
-G._init_(5,True)
-G.addEdge(0,1,1)
-G.addEdge(0,2,6)
-G.addEdge(1,2,2)
-G.addEdge(1,3,1)
-G.addEdge(2,3,2)
-G.addEdge(2,4,5)
-G.addEdge(3,4,5)
-G.shortestPath(0)
+G._init_(5,False)
 
-# if __name__ == '__main__':
+# G.addEdge(0,1,1)
+# G.addEdge(0,2,6)
+# G.addEdge(1,2,2)
+# G.addEdge(1,3,1)
+# G.addEdge(3,2,2)
+# G.addEdge(2,4,5)
+# G.addEdge(3,4,5)
+# G.shortestPath(0,4)
+
+G.addEdge(0,1,6)
+G.addEdge(0,3,1)
+G.addEdge(1,3,2)
+G.addEdge(1,2,5)
+G.addEdge(3,4,1)
+G.addEdge(1,4,2)
+G.addEdge(2,4,5)
+G.shortestPath(0,2)
+
 #     g = Graph()
 #     g._init_(5,False)
 #     g.graph = [[0, 2, 0, 6, 0],
